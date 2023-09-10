043e1c10418be505224fa4ca7476509f921e2e66
Sreehari Nagesh <sreeharinagesh@gmail.com>
2023-09-10 18:55:30 +0530

    cors middleware update.

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,3 @@ RUN python -m pip install --upgrade pip && \
     python -m pip install -r /tmp/requirements.txt
 
 COPY ./app app/
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 import langchain
 import openai
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langchain.indexes import VectorstoreIndexCreator
 
 API_KEY = os.environ.get("OPENAI_API_KEY")
@@ -12,6 +13,13 @@ loader = langchain.document_loaders.TextLoader("app/data.txt")
 index = VectorstoreIndexCreator().from_loaders([loader])
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/query")
